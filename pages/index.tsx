c7d1c18b4e3600f55bbcefd27ef3e94da5df7b01
tur1ng <mohammadali.ap.2000@gmail.com>
2021-05-21 23:57:18 +0430

    add CollectionsTypescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,11 @@
+import { DoublyLinkedList } from "collectionstypescript";
+import styled from "styled-components";
+
 export default function Home() {
   return <div>Home</div>;
 }
+
+const Board = styled.div`
+  display: grid;
+  grid-template-columns: repeat(10, 1fr);
+`;
